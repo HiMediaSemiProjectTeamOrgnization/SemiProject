bc48e8d68332d857dfa0c718cdf906a91ec9d02a
DHM <goals132@gmail.com>
2025-11-26 15:50:47 +0900

    [FEAT] 라우터 회원/비회원 선택부터 퇴실까지 임시 구현 완료

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ import uvicorn
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import test
+from routers import test, kiosk
 from database import create_tables
 
 @asynccontextmanager
@@ -13,6 +13,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(test.router)
+app.include_router(kiosk.router)
 
 app.add_middleware(
     CORSMiddleware,
