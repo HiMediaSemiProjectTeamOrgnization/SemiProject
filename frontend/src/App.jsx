2b69e41bd1e27e1bea4d8b6f564b550eb36ab54d
nitz7155 <nitz7155@gmail.com>
2025-11-28 03:16:57 +0900

    update develop

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,28 +2,14 @@ import { createBrowserRouter } from 'react-router-dom';
 import './App.css';
 import WebLayout from './web/components/WebLayout.jsx';
 import Home from './Home.jsx';
-import Test from './pages/Test.jsx';
-import KioskHome from './kiosk/pages/KioskHome.jsx'
 import KioskApp from './kiosk/KioskApp.jsx'
 import TicketList from './web/pages/TicketList.jsx';
 import Payments from './web/pages/Payment.jsx';
 import WebIndex from './web/pages/WebIndex.jsx';
 
-/*
-############# 라우터 추가 방법
-############# path는 경로 element는 컴포넌트
-############# 주석으로 페이지명 서술 권장
-############# 헤더, 푸터 적용하는 페이지면 children 안에 추가
-############# 헤더, 푸터 적용하면 안하는 페이지면 children 바깥, 바깥에 추가
-{
-    path: 'page',
-    element: <Page />
-},
-*/
 const router = createBrowserRouter([
     {
         path: '/',
-        index: true,
         element: <Home />,
     },
     {
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+      tailwindcss(),
+  ],
   server: {
     proxy: {
-      '/api': 'http://127.0.0.1:8000', // FastAPI 서버 주소
+      '/api': 'http://127.0.0.1:8080', // FastAPI 서버 주소
     },
   },
 });
