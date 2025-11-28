a496ba5e98d44aa1ee642be02684e827fb3a282b
knayo <dezxny@gmail.com>
2025-11-28 16:41:54 +0900

    [FEAT] 회원 로그인 라우터 연결 및 정보 띄우기

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,6 +25,6 @@ app.add_middleware(
 
 if __name__ == "__main__":
     uvicorn.run("main:app",
-                host="localhost",
+                host="0.0.0.0",
                 port=8080,
                 reload=True)
